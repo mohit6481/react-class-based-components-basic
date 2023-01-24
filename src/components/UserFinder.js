fafdd510dc1993484f69e724dca3901cb8401054
mohit6481 <mohit.rathi6481@gmail.com>
2023-01-24 18:09:56 +0530

    Error Boundary Component

--- src/components/UserFinder.js
+++ src/components/UserFinder.js
@@ -4,6 +4,7 @@ import { Component } from "react";
 import Users from "./Users";
 import classes from "./UserFinder.module.css";
 import UsersContext from "../store/users-context";
+import ErrorBoundary from "./ErrorBoundary";
 
 class UserFinder extends Component {
   static contextType = UsersContext;
@@ -42,7 +43,9 @@ class UserFinder extends Component {
         <div className={classes.finder}>
           <input type="search" onChange={this.searchChangeHandler.bind(this)} />
         </div>
-        <Users users={this.state.filteredUsers} />
+        <ErrorBoundary>
+          <Users users={this.state.filteredUsers} />
+        </ErrorBoundary>
       </>
     );
   }
